d5be83b27d73db899d63355b69b7e9d6a1c37d99
harrmitch <minhyh0987@gmail.com>
2022-08-18 16:03:37 +0700

    Implement assembler with symbols

--- assembler.py
+++ assembler.py
@@ -1,21 +1,42 @@
 import sys
 from modules.parser import Parser
+from modules.symbol_table import SymbolTable
 import modules.code as code
 
 # check usage
 if len(sys.argv) != 2:
     raise Exception("Usage: python assembler.py [FILENAME]")
 
+symbol_table = SymbolTable()
 parser = Parser(sys.argv[1])
 
-# parse and translate lines
+# first pass
+i = -1
+while parser.has_more_commands():
+    parser.advance()
+    match parser.command_type():
+        case "L_COMMAND":
+            symbol_table.add_entry(parser.symbol(), i + 1)
+        case other:
+            i += 1
+
+# second pass
+parser.reset()
 lines = []
+n = 16
 while parser.has_more_commands():
     parser.advance()
     match parser.command_type():
         case "A_COMMAND":
-            num = int(parser.symbol())
-            lines.append(f"{num:016b}")
+            symbol = parser.symbol()
+            if symbol.isdigit():
+                index = int(symbol)
+            else:
+                if not symbol_table.contains(symbol):
+                    symbol_table.add_entry(symbol, n)
+                    n += 1
+                index = symbol_table.get_address(symbol)
+            lines.append(f"{index:016b}")
         case "C_COMMAND":
             comp = code.comp(parser.comp())
             dest = code.dest(parser.dest())
